58f941d91c6c0d1c2d22cde4b8766edbda824196
Naethanial <107021680+Naethanial@users.noreply.github.com>
2025-03-22 19:19:28 -0700

    Update wal-polybar.py

--- wal-polybar.py
+++ wal-polybar.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import os
-from os.path import join
 import sys
+from os.path import join
 
 VERSION = '0.9'
 HOME = os.getenv('HOME')
@@ -10,64 +10,80 @@ POLYBAR_CONFIG_PATH = join(POLYBAR_CONFIG_FOLDER_PATH, 'config')
 POLYBAR_TEMPLATE_PATH = join(POLYBAR_CONFIG_FOLDER_PATH, 'config.template')
 WAL_CACHE_PATH = join(HOME, '.cache/wal/colors')
 
-
-def wal_cache_file_to_dict():  # Creating a list where each element is a color from the wal cache
-    with open(WAL_CACHE_PATH, 'r') as file:
-        colors = []
-        for index, line in enumerate(file.readlines()):
-            colors.append(('${{wal.color{}}}'.format(index), line[:-1]))
+def wal_cache_file_to_dict():
+    """Load the pywal colors from the cache file and return a list of placeholder/color tuples."""
+    try:
+        with open(WAL_CACHE_PATH, 'r') as file:
+            colors = []
+            for index, line in enumerate(file.readlines()):
+                # Remove any trailing whitespace/newline characters
+                colors.append(('${{wal.color{}}}'.format(index), line.strip()))
+    except IOError as e:
+        print('Error reading WAL cache file ({}): {}'.format(WAL_CACHE_PATH, e))
+        sys.exit(1)
     return colors
 
+def modify_polybar_config_file(colors):
+    """Replace placeholders in the polybar template with colors from pywal and write the new config."""
+    try:
+        with open(POLYBAR_TEMPLATE_PATH, 'r') as template_file:
+            template_lines = template_file.readlines()
+    except IOError as e:
+        print('Error reading the Polybar template file ({}): {}'.format(POLYBAR_TEMPLATE_PATH, e))
+        sys.exit(1)
 
-def modify_polybar_config_file(colors):  #
-    with open(POLYBAR_TEMPLATE_PATH, 'r') as template_file:
+    try:
         with open(POLYBAR_CONFIG_PATH, 'w') as config_file:
-            for line in template_file.readlines():
-                for i, x in colors:
-                    if i in line:
-                        line = line.replace(i, x)
+            for line in template_lines:
+                for placeholder, color in colors:
+                    if placeholder in line:
+                        line = line.replace(placeholder, color)
                 config_file.write(line)
-
+    except IOError as e:
+        print('Error writing the Polybar config file ({}): {}'.format(POLYBAR_CONFIG_PATH, e))
+        sys.exit(1)
 
 def main():
     global POLYBAR_TEMPLATE_PATH, WAL_CACHE_PATH, VERSION
-    if len(sys.argv) == 2:
+
+    # Handle command-line arguments
+    if len(sys.argv) > 1:
         if sys.argv[1] == '-v':
             print('wal-polybar ' + VERSION)
-            sys.exit()
-        if sys.argv[1] == '-h' or sys.argv[1] == '--help':
-            print("usage: wal-polybar [-h] [-v version] [-t 'path/to/template']\n")
-            print('optional arguments:')
-            print('\t-h, --help            show this help message and exit')
-            print('\t-v                    displays the version of wal-polybar')
-            print('\t-t                    runs the script with a custom template path')
-            sys.exit()
+            sys.exit(0)
+        elif sys.argv[1] in ['-h', '--help']:
+            print("Usage: wal-polybar [-h] [-v] [-t 'path/to/template']\n")
+            print("Optional arguments:")
+            print("\t-h, --help   Show this help message and exit")
+            print("\t-v           Display the version of wal-polybar")
+            print("\t-t           Use a custom template for polybar")
+            sys.exit(0)
+        elif '-t' in sys.argv:
+            t_index = sys.argv.index('-t')
+            if t_index + 1 < len(sys.argv):
+                POLYBAR_TEMPLATE_PATH = sys.argv[t_index + 1]
+                if not os.path.isfile(POLYBAR_TEMPLATE_PATH):
+                    print('The provided template file does not exist: {}'.format(POLYBAR_TEMPLATE_PATH))
+                    sys.exit(1)
+            else:
+                print("Missing template path after -t")
+                sys.exit(1)
         else:
             print('Syntax error!')
-            sys.exit()
-    if len(sys.argv) >= 3:
-        if sys.argv.index('-t') != -1:
-            POLYBAR_TEMPLATE_PATH = sys.argv[sys.argv.index('-t') + 1]
-            try:
-                open(POLYBAR_TEMPLATE_PATH, 'r')
-            except IOError:
-                print('The provided template file does not exist')
-                sys.exit()
+            sys.exit(1)
+
+    # Check if the WAL cache file exists
+    if not os.path.isfile(WAL_CACHE_PATH):
+        print('Could not find the WAL cache file at {}.'.format(WAL_CACHE_PATH))
+        print('Please run pywal to generate the colors file.')
+        sys.exit(1)
+
     print('Loading wal cache...')
-    try:
-        colors = wal_cache_file_to_dict()
-    except IOError:
-        print('Could not read ' + WAL_CACHE_PATH)
-        sys.exit()
+    colors = wal_cache_file_to_dict()
     print('Wal cache successfully loaded!')
     print('Loading the template and generating the new config...')
-    try:
-        modify_polybar_config_file(colors)
-    except IOError:
-        print('Could not read ' + WAL_CACHE_PATH)
-        sys.exit()
+    modify_polybar_config_file(colors)
     print('Template file successfully loaded and new config file generated!')
-
 
 if __name__ == '__main__':
     main()
